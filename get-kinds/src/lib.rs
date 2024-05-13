4e762b80f3f81b876e593ff6c8b728fd52731f95
Ishaan Subramanya <whthownothing@gmail.com>
2024-05-13 21:57:46 +0530

    docs

--- get-kinds/src/lib.rs
+++ get-kinds/src/lib.rs
@@ -1,4 +1,16 @@
+/// Implements just one function [`Kind::kind`]
 pub trait Kind {
+    /// ### Examples
+    /// ```
+    /// use generate_kinds::kinds;
+    /// 
+    /// #[kinds]
+    /// enum Test {
+    ///     T1,
+    ///     T2
+    /// }
+    /// assert_eq!(Test::T1.kind(), "Test :: T1");
+    /// ```
     fn kind<'a>(&self) -> &'a str;
 }
 
